471596a6ee19b09f2510b59f4b6838a8d1cc4162
minnyww <minrrverse@gmail.com>
2022-03-16 14:27:21 +0700

    [add] adense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import GlobalStyle from "../styles/GlobalStyles";
 import "../styles/globals.css";
 import { NextUIProvider, createTheme } from "@nextui-org/react";
+import Script from "next/script";
 // import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const darkTheme = createTheme({
@@ -18,10 +19,20 @@ function MyApp({ Component, pageProps }: AppProps) {
     //     dark: darkTheme.className,
     //   }}
     // >
-    <NextUIProvider theme={darkTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <NextUIProvider theme={darkTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </NextUIProvider>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-9048050181020642"
+        async
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9048050181020642"
+        crossOrigin="anonymous"
+      />
+    </>
     // </NextThemesProvider>
   );
 }
